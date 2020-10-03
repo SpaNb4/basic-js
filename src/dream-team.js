2cc6902c593e09c45592a0d949a638cc124d4f38
SpaNb4 <spanb4@gmail.com>
2020-10-03 19:55:25 +0300

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (typeof (members) == 'array' || typeof (members) == 'object' && members != null && members['foo'] !='bar') {
+    let dreamTeam = '';
+    for (let i = 0; i < members.length; i++) {
+      if (typeof (members[i]) == 'string') {
+        members[i] = members[i].trim()[0].toUpperCase();
+      }
+    }
+    members.sort();
+    for (let i = 0; i < members.length; i++) {
+      if (typeof (members[i]) == 'string') {
+
+        dreamTeam += members[i][0];
+      }
+    }
+    return dreamTeam;
+  }
+  return false;
+}
