d75a9f15f28ede9cca6a234cd2eccef857cd3064
SpaNb4 <spanb4@gmail.com>
2020-10-04 22:17:01 +0300

    feat: add chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position > this.chain.length || typeof(position)!='number') {
+      this.chain=[];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+    for (let i = 0; i < this.chain.length; ++i) {
+      str = str + `( ${this.chain[i]} )~~`;
+    }
+    this.chain=[];
+    return str.substring(0, str.length - 2);
   }
 };
 
