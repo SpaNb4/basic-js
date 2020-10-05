cf7ecfddb66f7c00e3a33415064a445975cf459f
SpaNb4 <spanb4@gmail.com>
2020-10-05 19:04:51 +0300

    feat: add VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,103 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.direct = direct;
+  }
+
+  encrypt(text, key) {
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let l = 0;
+    if (text.length > key.length) {
+      while (key.length != text.length) {
+        key = key + key[l];
+        l++;
+      }
+    }
+    let res = '';
+    let space = false;
+    let n = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] != ' ') {
+        if (/^[A-Z]+$/.test(text[i])) {
+          res += this.alphabet.charAt((text[i].charCodeAt() + (key[i - n].charCodeAt())) % this.alphabet.length);
+        }
+        else {
+          res += text[i];
+        }
+      }
+      else {
+        space = true;
+        n++;
+        i++;
+        if (text[i + 1] != ' ') {
+          if (/^[A-Z]+$/.test(text[i + 1])) {
+            res += ' ' + this.alphabet.charAt((text[i].charCodeAt() + (key[i - n].charCodeAt())) % this.alphabet.length);
+          }
+          else {
+            res += ' ' + text[i];
+          }
+        }
+        else {
+          res += ' ';
+        }
+      }
+    }
+    if (this.direct) {
+      return res;
+    }
+    else {
+      return res.split("").reverse().join("");
+    }
+  }
+
+  decrypt(text, key) {
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let l = 0;
+    if (text.length > key.length) {
+      while (key.length != text.length) {
+        key = key + key[l];
+        l++;
+      }
+    }
+    let res = '';
+    let space = false;
+    let n = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] != ' ') {
+        if (/^[A-Z]+$/.test(text[i])) {
+          res += this.alphabet.charAt((text[i].charCodeAt() - (key[i - n].charCodeAt() - this.alphabet.length)) % this.alphabet.length);
+        }
+        else {
+          res += text[i];
+        }
+      }
+      else {
+        space = true;
+        n++;
+        i++;
+        if (text[i + 1] != ' ') {
+          if (/^[A-Z]+$/.test(text[i + 1])) {
+            res += ' ' + this.alphabet.charAt((text[i].charCodeAt() - (key[i - n].charCodeAt() - this.alphabet.length)) % this.alphabet.length);
+          }
+          else {
+            res += ' ' + text[i];
+          }
+        }
+        else {
+          res += ' ';
+        }
+      }
+    }
+    if (this.direct) {
+      return res;
+    }
+    else {
+      return res.split("").reverse().join("");
+    }
   }
 }
 
